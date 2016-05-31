dfa95d90e60a1264300fab645bac7dfdd8a2bafb
pinottianderson <pinotti.anderson@gmail.com>
2016-05-31 16:08:46 -0300

    Aulas 16 a 18.

--- estruturadados/src/com/loiane/estruturadados/pilha/Pilha.java
+++ estruturadados/src/com/loiane/estruturadados/pilha/Pilha.java
@@ -16,9 +16,24 @@ public class Pilha<T> extends EstruturaEstatica<T> {
 		super.adiciona(elemento);		
 	}
 	
+	public T topo(){
+		if (this.estaVazia()){
+			return null;
+		}else{
+			return this.elementos[this.tamanho-1];
+		}
+	}
 	
-	
-	
+	public T desempilha(){
+		if (this.estaVazia()){
+			return null;
+		}else{
+			T elemento = this.elementos[this.tamanho-1];
+			this.tamanho--;
+			
+			return elemento;
+		}
+	}
 	
 
 }
